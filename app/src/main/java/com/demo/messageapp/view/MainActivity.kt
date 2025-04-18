4e2afe274f77a2c203aadb03c64653a0997a1aaa
dunglqmblol <dunghthlqt1995@gmail.com>
2025-04-18 18:41:40 +0700

    feat(firebase): configure Firebase services

--- app/src/main/java/com/demo/messageapp/view/MainActivity.kt
+++ app/src/main/java/com/demo/messageapp/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.demo.messageapp
+package com.demo.messageapp.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.demo.messageapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
