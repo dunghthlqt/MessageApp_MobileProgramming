83ed79832c73a5358ac6eb1490adf949fb0df1db
dunglqmblol <dunghthlqt1995@gmail.com>
2025-04-25 06:11:52 +0700

    feat: Add create new user in AuthRepository, ViewModel

--- app/src/main/java/com/demo/messageapp/model/User.kt
+++ app/src/main/java/com/demo/messageapp/model/User.kt
@@ -2,5 +2,8 @@ package com.demo.messageapp.model
 
 data class User (
     val uid: String = "",
-    val email: String = ""
+    val email: String = "",
+    val displayName: String = "",
+    val avatarUrl: String = "",
+    val isOnline: Boolean = false
 )
--- app/src/main/java/com/demo/messageapp/repository/AuthRepository.kt
+++ app/src/main/java/com/demo/messageapp/repository/AuthRepository.kt
@@ -1,11 +1,13 @@
 package com.demo.messageapp.repository
 
+import com.demo.messageapp.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AuthRepository {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     fun register(email: String, password: String, callback: (Boolean, String?) -> Unit) {
         auth.createUserWithEmailAndPassword(email, password)
@@ -57,5 +59,20 @@ class AuthRepository {
                       callback(false, task.exception?.message)
                   }
               }
+    }
+
+    fun createUserProfile(callback: (Boolean, String?) -> Unit) {
+        val user = User (
+            uid = auth.currentUser!!.uid,
+            email = auth.currentUser!!.email ?: ""
+        )
+        db.collection("users").document(auth.currentUser!!.uid)
+            .set(user)
+            .addOnSuccessListener { _ ->
+                callback(true, null)
+            }
+            .addOnFailureListener { e ->
+                callback(false, e.message)
+            }
     }
 }
