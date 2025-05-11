a0d13d596c110321f05bc573c517f34e4eca3af0
dunglqmblol <dunghthlqt1995@gmail.com>
2025-05-11 09:30:24 +0700

    ui: Implement contacts activity

--- app/src/main/java/com/demo/messageapp/model/User.kt
+++ app/src/main/java/com/demo/messageapp/model/User.kt
@@ -7,7 +7,5 @@ data class User (
     val avatarUrl: String = "",
     val isOnline: Boolean = false,
     val joinedConversation: List<String> = listOf(),
-
-    //duc them cai nay
-    val messages: List<Message> = listOf()
+    val contatcs: List<String> = listOf()
 )
--- app/src/main/java/com/demo/messageapp/view/ContactsActivity.kt
+++ app/src/main/java/com/demo/messageapp/view/ContactsActivity.kt
@@ -5,19 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.demo.messageapp.R
 import com.demo.messageapp.databinding.ActivityContactsBinding
 import com.demo.messageapp.databinding.ActivityHomeBinding
+import com.demo.messageapp.view.adapter.ContactsAdapter
+import com.demo.messageapp.view.adapter.ConversationAdapter
 
 class ContactsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityContactsBinding
+    private lateinit var adapter: ContactsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityContactsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        adapter = ContactsAdapter(emptyList()) { contact ->
+            // TODO: Xử lý khi người dùng nhấn vào 1 cuộc trò chuyện
+        }
+
+        binding.conversationRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.conversationRecyclerView.adapter = adapter
 
 
     }
