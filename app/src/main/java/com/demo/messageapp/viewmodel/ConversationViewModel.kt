289e8eb3de0f8dbc3292c9c05b59cea4746e1b95
dunglqmblol <dunghthlqt1995@gmail.com>
2025-05-12 14:22:00 +0700

    Implement New Contact Screen layout and Add Message

--- app/src/main/java/com/demo/messageapp/viewmodel/ConversationViewModel.kt
+++ app/src/main/java/com/demo/messageapp/viewmodel/ConversationViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.demo.messageapp.model.resultmodel.CreateConversationResult
 import com.demo.messageapp.model.resultmodel.GetConversationListResult
+import com.demo.messageapp.model.resultmodel.GetConversationTwoUIDResult
 import com.demo.messageapp.repository.ConversationRepository
 
 class ConversationViewModel : ViewModel() {
@@ -12,6 +13,7 @@ class ConversationViewModel : ViewModel() {
     val createConversationResult = MutableLiveData<CreateConversationResult>()
     val deleteConversationResult = MutableLiveData<Pair<Boolean, String?>>()
     val getConversationListResult = MutableLiveData<GetConversationListResult>()
+    val getConversationTwoUidResult = MutableLiveData<GetConversationTwoUIDResult>()
     
     fun createConversation(participantIds: List<String>, conversationName: String, currentUserId: String) {
         repository.createConversation(participantIds, conversationName, currentUserId) {success, message, conversationId ->
@@ -27,5 +29,25 @@ class ConversationViewModel : ViewModel() {
         repository.getConversationList(userUid) {success, message, conversationList ->
             getConversationListResult.value = GetConversationListResult(success, message, conversationList)
         }
+    }
+    fun getConversationTwoUID(uid1: String, uid2: String) {
+        repository.getConversationList(uid2) {success, message, conversationList ->
+            val conversation = conversationList!!.firstOrNull { conversation ->
+                val participantIds = conversation.participantIds ?: emptyList()
+
+                participantIds.size == 2 &&
+                        participantIds.contains(uid1) &&
+                        participantIds.contains(uid2)
+            }
+            getConversationTwoUidResult.value = GetConversationTwoUIDResult(success, message, conversation?.id)
+        }
+    }
+    fun resetConversationTwoUidResult() {
+        // You should adjust this implementation according to your ConversationResult class structure
+        getConversationTwoUidResult.value = GetConversationTwoUIDResult(
+            success = false,
+            errorMessage = "",
+            conversationId = ""
+        )
     }
 }
